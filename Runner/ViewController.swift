10eacfe98c2db417f8c16578d324a1b833a71f8f
Macbook <macbook@nikolay-macbook.dlink>
2019-11-24 18:30:48 +0200

    Initial Commit

---
+++ Runner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Runner
+//
+//  Created by Macbook on 24.11.2019.
+//  Copyright © 2019 Nikolay_Romanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
